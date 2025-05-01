7df9e88cd3ab0e35a5132fcc7d80a0f2bc27fadd
vaishali896 <vaishali0507mishra@gmail.com>
2025-05-01 18:43:20 +0530

    Update Crowdfunding.sol

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract CarbonCreditTracker {
+    mapping(address => uint256) public credits;
+
+    // Issue carbon credits to a company
+    function issueCredits(address to, uint256 amount) public {
+        credits[to] += amount;
+    }
+
+    // Use or burn carbon credits
+    function useCredits(uint256 amount) public {
+        require(credits[msg.sender] >= amount, "Insufficient credits");
+        credits[msg.sender] -= amount;
+    }
+}
